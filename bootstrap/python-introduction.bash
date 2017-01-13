3da49a13c2fdb7a4451db0e768496eb143428d58
Jyry Suvilehto <jyry.suvilehto@csc.fi>
2017-01-13 16:54:46 +0200

    retrieve custom.js

--- bootstrap/python-introduction.bash
+++ bootstrap/python-introduction.bash
@@ -7,3 +7,6 @@ cd /home/jovyan/work
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 git clone https://github.com/csc-training/python-introduction.git
+
+mkdir -p ~/.jupyter/custom/
+wget https://raw.githubusercontent.com/csc-training/python-introduction/gh-pages/assets/custom.js -O ~/.jupyter/custom/custom.js
